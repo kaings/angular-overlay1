bf2461ea97265322fa77015c5111601c627b21ec
willys_tjiawi <willys@huubap.com>
2019-02-10 23:02:18 +0800

    using overlay to display overaly content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  isOpen: boolean = false;
+
+  colors = [
+    {color: '#F44336', name: 'red'},
+    {color: '#E91E63', name: 'pink'},
+    {color: '#9C27B0', name: 'purple'},
+    {color: '#3F51B5', name: 'blue'},
+    {color: '#009688', name: 'green'},
+    {color: '#FFEB3B', name: 'yellow'},
+    {color: '#FF9800', name: 'orange'},
+    {color: '#795548', name: 'brown'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {A11yModule} from '@angular/cdk/a11y';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OverlayModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
